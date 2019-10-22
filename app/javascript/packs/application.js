e61fdfc21257ce572e11e415a635de036237d1af
Dumez Theo <dumez.theo.h@gmail.com>
2019-10-22 12:32:01 +0200

    images banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,19 @@
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
+
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+var links = ["./assets/two_faces.jpg","./assets/banerbleue"];
+var images = ["./assets/two_faces.jpg","./assets/banerbleue.jpg"];
+var i = 0;
+var renew = setInterval(function(){
+  if(links.length == i){
+      i = 0;
+  }
+  else {
+  document.getElementById("banner").src = images[i];
+  i++;
+  }
+},5000);
