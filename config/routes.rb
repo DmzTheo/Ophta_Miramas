5cee1000f44840000cae8701d6113cc0c66aa24c
Dumez Theo <dumez.theo.h@gmail.com>
2019-10-27 11:53:12 +0100

    référencement

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,12 @@ Rails.application.routes.draw do
   resources :preparerlinterventions, only: [:show]
   resources :consignepostoperatoires, only: [:show]
   resources :choisissezvotreimplants, only: [:show]
+
+  get "/404", to: "errors#not_found", via: :all
+  get "/422", to: "errors#unacceptable", via: :all
+  get "/500", to: "errors#internal_server_error", via: :all
+
+  # 301 redirect from old URLs
+  get "/old_path_to_posts/:id", to: redirect("/posts/%{id}s")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/errors_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ErrorsControllerTest < ActionDispatch::IntegrationTest
+  test "should get not_found" do
+    get errors_not_found_url
+    assert_response :success
+  end
+
+  test "should get unacceptable" do
+    get errors_unacceptable_url
+    assert_response :success
+  end
+
+  test "should get internal_server_error" do
+    get errors_internal_server_error_url
+    assert_response :success
+  end
+
+end
