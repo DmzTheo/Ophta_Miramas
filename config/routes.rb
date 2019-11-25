8372d99b3eb63f234eca7cc3a08a905f6a2a93e3
Dumez Theo <dumez.theo.h@gmail.com>
2019-11-25 11:49:19 +0100

    dynamic errors pages

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ Rails.application.routes.draw do
     resource :choisissezvotreimplant, only: [:show]
   end
 
-  get '/404', to: 'errors#not_found', via: :all
-  get '/422', to: 'errors#unacceptable', via: :all
-  get '/500', to: 'errors#internal_server_error', via: :all
+  match '/404', to: 'errors#not_found', via: :all
+  match '/422', to: 'errors#unacceptable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
